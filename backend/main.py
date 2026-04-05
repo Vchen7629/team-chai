4937cdb9ebdb0be74da03b179e5938860ee38e50
Vchen7629 <chenvincent7629@gmail.com>
2026-04-05 11:42:52 -0700

    feat(backend): created basic main.py that starts fastapi uvicorn server with one route

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+async def hello_world() -> None:
+    return {"message": "hello worlddddd!"}
 
 if __name__ == "__main__":
-    main()
+
+    uvicorn.run(app, host="0.0.0.0", port=8001)
