482e9dbafa0163937ae29fba3103993fd08ce2cb
Vchen7629 <chenvincent7629@gmail.com>
2026-04-09 21:32:36 -0700

    chore(backend): added user_sessions table to the testing dn

--- test-database/init.sql
+++ test-database/init.sql
@@ -1,5 +1,11 @@
 CREATE TABLE IF NOT EXISTS user_login (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_sessions (
+    username TEXT PRIMARY KEY REFERENCES user_login(username),
+    session_token TEXT NOT NULL,
+    expire_at TIMESTAMPTZ
 );
