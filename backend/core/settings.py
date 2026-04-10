de17529d6528c42678f4d9be6285f3fc369b31fc
Vchen7629 <chenvincent7629@gmail.com>
2026-04-09 20:11:30 -0700

    feat(backend): created settings.py for the api configs

---
+++ backend/core/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    """Api settings/configs live here"""
+    FASTAPI_PORT: int = 8001
+    
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "team-chai"
+    DB_USER: str = "postgres"
+    DB_PASS: str = "password"
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+settings = Settings()
