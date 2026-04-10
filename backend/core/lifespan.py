6d7620bba904d67d7a32842b91eb06fdea24cd1d
Vchen7629 <chenvincent7629@gmail.com>
2026-04-09 21:29:56 -0700

    feat(backend): created lifespan.py to control db connection startup/cleanup

---
+++ backend/core/lifespan.py
@@ -0,0 +1,23 @@
+from typing import Any
+from fastapi import FastAPI
+from sqlalchemy import text
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
+from db.connection import engine
+import os
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[Any, Any]:
+    # checking database is reachable before proceeding
+    try:
+        async with engine.connect() as conn:
+            await conn.execute(text("SELECT 1"))
+    except Exception as e:
+        print(f"failed to connect to db with error: {str(e)}")
+        await engine.dispose()
+        os._exit(1)
+
+    yield
+
+    await engine.dispose()
+
