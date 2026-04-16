2f2d3adfea387b99111ea8abb4e12502d7c06f34
Vchen7629 <chenvincent7629@gmail.com>
2026-04-15 22:35:28 -0700

    feat(frontend): added api helper for signup

--- frontend/api/auth.ts
+++ frontend/api/auth.ts
@@ -7,5 +7,17 @@ export const AuthService = {
         const response = await client.post(`/auth/login`, { username, password })
         await SecureStore.setItemAsync('session_token', response.data.session_token)
         return response.data
-    }
+    },
+
+    signup: async(
+        username: string, email: string, password: string, 
+        age: string, weight: string, heightFT: string, heightIn: string, gender: string, 
+        activityLevel: string, stepGoal: string
+    ) => {
+        const response = await client.post(`/auth/signup`, {
+            username, email, password, age, weight, heightFT, heightIn, gender, activityLevel, "targetsteps": stepGoal
+        })
+
+        return response.data
+    }   
 }
