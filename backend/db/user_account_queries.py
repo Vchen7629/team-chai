06d4eb238fa3c126ea7605ca0f6553b4b6b51ecf
Vchen7629 <chenvincent7629@gmail.com>
2026-04-15 22:33:22 -0700

    feat(backend): updated sql signup query function to write to user_profile table

--- backend/db/user_account_queries.py
+++ backend/db/user_account_queries.py
@@ -1,5 +1,7 @@
+from routes.models import UserSignUpRequest
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
+import bcrypt
 
 async def fetch_hashed_password(session: AsyncSession, username: str) -> str:
     """Fetch hashed password for the user account via username"""
@@ -16,14 +18,44 @@ async def fetch_hashed_password(session: AsyncSession, username: str) -> str:
 
     return row[0]
 
-async def create_new_user_account(session: AsyncSession, username: str, hashed_password: str) -> None:
+async def create_new_user_account(session: AsyncSession, user_data: UserSignUpRequest) -> None:
     """Create a new user account with the username and hashed password (bcrypt) in db"""
-    query = """
-    INSERT INTO user_login (username, hashed_password) 
-    VALUES (:username, :hashed_password)
-    ON CONFLICT (username) DO NOTHING
+    hash_password = bcrypt.hashpw(user_data.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    user_login_query = """
+        INSERT INTO user_login (username, hashed_password) 
+        VALUES (:username, :hashed_password)
+        ON CONFLICT (username) DO NOTHING
     """
 
-    result = await session.execute(text(query), {"username": username, "hashed_password": hashed_password})
-    if result.rowcount == 0:
+    user_login_result = await session.execute(
+        text(user_login_query),
+        {"username": user_data.username, "hashed_password": hash_password}
+    )
+    if user_login_result.rowcount == 0:
         raise ValueError("Username already taken")
+
+    user_profile_query = """
+        INSERT INTO user_profile 
+        (username, email, age, weight, heightin, gender, activitylevel, targetsteps)
+        VALUES
+        (:username, :email, :age, :weight, :heightin, :gender, :activitylevel, :targetsteps)
+        ON CONFLICT (username) DO NOTHING
+    """
+
+    # con
+    heightin_conversion = user_data.heightFT * 12 + user_data.heightIn
+    
+    await session.execute(
+        text(user_profile_query),
+        {
+            "username": user_data.username,
+            "email": user_data.email,
+            "age": user_data.age,
+            "weight": user_data.weight,
+            "heightin": heightin_conversion,
+            "gender": user_data.gender,
+            "activitylevel": user_data.activityLevel,
+            "targetsteps": user_data.targetsteps,
+        }
+    )
