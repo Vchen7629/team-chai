dbaf43e2e924c227193022be4e22fdf84a407359
Vchen7629 <chenvincent7629@gmail.com>
2026-04-22 13:41:25 -0700

    chore(test-database): created user_daily_steps table in sql schema

--- test-database/init.sql
+++ test-database/init.sql
@@ -20,3 +20,11 @@ CREATE TABLE IF NOT EXISTS user_profile (
     activitylevel TEXT NOT NULL,
     targetsteps INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS user_daily_steps {
+    username  TEXT REFERENCES user_login(username),
+    steps     INTEGER DEFAULT 0,
+    curr_date DATE NOT NULL,
+
+    PRIMARY KEY (username, curr_date)
+}
