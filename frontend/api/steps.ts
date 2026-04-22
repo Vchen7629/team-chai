0b22c8f4080754cab6e8ff14b6f93ee2f1755e6c
Vchen7629 <chenvincent7629@gmail.com>
2026-04-22 16:56:33 -0700

    refactor(frontend): updated api steps.ts to fetch session token for localstorage and send it to the api instead of passing username

--- frontend/api/steps.ts
+++ frontend/api/steps.ts
@@ -1,20 +1,27 @@
 import client from "./client"
+import * as SecureStore from 'expo-secure-store'
 
 export const StepsService = {
-    create_new_steps: async(username: string, steps: number) => {
-        const response = await client.post(`/steps/new`, { username, steps })
+    create_new_steps: async(steps: number) => {
+        const session_token = await SecureStore.getItemAsync('session_token')
+
+        const response = await client.post(`/steps/new`, { session_token, steps })
 
         return response.data
     },
 
-    update_user_steps: async(username: string, steps: number) => {
-        const response = await client.post(`/steps/add`, { username, steps })
+    update_user_steps: async(steps: number) => {
+        const session_token = await SecureStore.getItemAsync('session_token')
+
+        const response = await client.post(`/steps/add`, { session_token, steps })
 
         return response.data
     },
 
-    fetch_user_steps: async(username: string, date: string) => {
-        const response = await client.post(`/steps/get`, { username, date})
+    fetch_user_steps: async(date: string) => {
+        const session_token = await SecureStore.getItemAsync('session_token')
+
+        const response = await client.post(`/steps/get`, { session_token, date})
 
         return response.data
     }   
