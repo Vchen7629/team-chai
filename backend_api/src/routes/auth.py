a6ffbeab003241bdbb0dcc9abfb8dfc250b1531c
Vchen7629 <chenvincent7629@gmail.com>
2026-04-23 15:44:33 -0700

    chore(backend_api): added structured logging to the api routes

--- backend_api/src/routes/auth.py
+++ backend_api/src/routes/auth.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from core.logging import logger
 from routes.models import UserSignUpRequest
 from db.session_queries import create_new_user_session
 from db.user_account_queries import fetch_hashed_password
@@ -34,16 +35,19 @@ async def user_login(
     try:
         hashed_password = await fetch_hashed_password(db, request.username)
     except ValueError:
+        logger.error("No user account found for the username")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="user not found"
         )
-    except Exception:
+    except Exception as e:
+        logger.error("unknown error fetching hashed password", err=str(e))
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     password_match = bcrypt.checkpw(
         request.password.encode("utf-8"), hashed_password.encode("utf-8")
     )
     if password_match is False:
+        logger.warn("password does not match with hashed password from db")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="password does not match"
         )
@@ -53,7 +57,8 @@ async def user_login(
     try:
         await create_new_user_session(db, request.username, session_token)
 
-    except Exception:
+    except Exception as e:
+        logger.error("unknown error creating new user session", err=str(e))
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     return UserLoginResponse(message="login successful!", session_token=session_token)
@@ -67,12 +72,15 @@ async def user_signup(
     try:
         await create_new_user_account(db, request)
     except ValueError as e:
+        logger.error("error creating new user account", err=str(e))
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
     except IntegrityError:
+        logger.error("account already exists in db")
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="account already exists"
         )
-    except Exception:
+    except Exception as e:
+        logger.error("unknown error creating new user account", err=str(e))
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     return {"message": "successfully created new user!"}
