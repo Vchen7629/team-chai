425c1c51ec4b51c13f76ccab9d9d0d0447cfa9d1
Vchen7629 <chenvincent7629@gmail.com>
2026-04-23 16:17:49 -0700

    chore(backend_api): added type validation to signup pydantic model

--- backend_api/src/routes/models.py
+++ backend_api/src/routes/models.py
@@ -5,22 +5,21 @@ from pydantic import BaseModel
 
 
 class UserSignUpRequest(BaseModel):
-    username: str
-    email: str
-    password: str
-    age: int
-    weight: int
-    heightFT: int
-    heightIn: int
-    gender: str
-    activityLevel: str
+    username: str = Field(min_length=1)
+    email: str = Field(min_length=4)
+    password: str = Field(min_length=1)
+    age: int = Field(ge=13, le=70)
+    weight: int = Field(gt=0)
+    heightFT: int = Field(ge=4, le=7)
+    heightIn: int = Field(gt=0, le=12)
+    gender: Literal["male", "female", "other", "prefer_not_to_say"]
+    activityLevel: Literal["sedentary", "light", "moderate", "active", "very_active"]
 
 
 class UserFitnessData(BaseModel):
     age: int = Field(ge=13, le=70)
     weight: int = Field(gt=0)
-    heightFT: int = Field(ge=4, le=7)
-    heightIn: int = Field(gt=0, le=12)
+    heightin: int = Field(gt=54, le=84)
     gender: Literal["male", "female", "other", "prefer_not_to_say"]
     activityLevel: Literal["sedentary", "light", "moderate", "active", "very_active"]
     avg_steps_7_days: float = Field(ge=0)
