bde2fbbefc309c2a3e077374d2f5ad070b64dd58
Vchen7629 <chenvincent7629@gmail.com>
2026-04-27 15:06:02 -0700

    feat(backend_api): created route and sql query to fetch username and email

--- backend_api/src/db/user_queries.py
+++ backend_api/src/db/user_queries.py
@@ -3,6 +3,24 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.logging import logger
 from routes.models import UserFitnessData
 
+async def fetch_email(
+    db_session: AsyncSession, username: str
+) -> str:
+    """Fetch email from db"""
+    query = """
+        SELECT email FROM user_profile 
+        WHERE LOWER(username) = LOWER(:username)
+    """
+
+    result = await db_session.execute(text(query), {"username": username})
+
+    row = result.fetchone()
+    if not row:
+        raise ValueError("No email found for username")
+
+    logger.debug("fetched email from ser_profile db table")
+
+    return row[0]
 
 async def fetch_fitness_data(
     db_session: AsyncSession, username: str
