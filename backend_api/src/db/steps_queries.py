280fa407d65ef88bcb931fa12e3289c2a350f362
Vchen7629 <chenvincent7629@gmail.com>
2026-04-27 22:38:45 -0700

    refactor(backend_api): updated sql query and route to update the correct fields

--- backend_api/src/db/steps_queries.py
+++ backend_api/src/db/steps_queries.py
@@ -5,55 +5,34 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.logging import logger
 
 
-async def insert_user_new_steps(session: AsyncSession, username: str, steps: int) -> None:
+async def insert_user_new_step_goal(session: AsyncSession, username: str, target_steps: int) -> None:
     """
-    creates new steps for a user for a given date. used when the user signs up
+    creates new step goal for a user for a given date. used when the user signs up
     """
-    if steps <= 0:
-        raise ValueError("steps must be positive")
+    if target_steps <= 0:
+        raise ValueError("target steps must be positive")
 
     query = """
-        INSERT INTO user_daily_steps (username, steps, curr_date) 
-        VALUES (:username, :steps, CURRENT_DATE)
+        INSERT INTO user_daily_steps (username, target_steps, curr_date) 
+        VALUES (:username, :target_steps, CURRENT_DATE)
         ON CONFLICT (username, curr_date) DO UPDATE SET
-            steps = EXCLUDED.steps;
+            target_steps = EXCLUDED.target_steps;
     """
 
-    await session.execute(text(query), {"username": username.strip(), "steps": steps})
+    await session.execute(text(query), {"username": username.strip(), "target_steps": target_steps})
 
-    logger.debug("created new steps record for the user in user_daily_steps db table")
+    logger.debug("created new target steps record for the user in user_daily_steps db table")
 
-
-async def update_user_steps(session: AsyncSession, username: str, steps: int, date: datetime) -> None:
-    """
-    Add new steps for a user for a given date. Used for the background step
-    tracker to increase daily steps
-    """
-    if steps <= 0:
-        raise ValueError("steps must be positive")
-
-    query = """
-        INSERT INTO user_daily_steps (username, steps, curr_date)
-        VALUES (:username, :steps, CURRENT_DATE)
-        ON CONFLICT (username, curr_date) DO UPDATE SET
-            steps = user_daily_steps.steps + EXCLUDED.steps;
-    """
-
-    await session.execute(text(query), {"username": username, "steps": steps})
-
-    logger.debug("updated steps record for the user in user_daily_steps db table")
-
-
-async def get_curr_date_steps(
+async def get_steps_goal_count(
     session: AsyncSession, username: str, date: date
 ) -> int:
     """
-    Fetch the current steps for the user for the current date, Used by the frontend to
-    display the steps for the current day like in the calendar component
+    Fetch the step goal for the user for the current date, Used by the frontend to
+    display the steps for the current day in user feed screen
     """
 
     query = """
-        SELECT steps FROM user_daily_steps
+        SELECT target_steps FROM user_daily_steps
         WHERE LOWER(username) = LOWER(:username)
             AND curr_date = :date;
     """
@@ -64,9 +43,55 @@ async def get_curr_date_steps(
 
     row = result.fetchone()
     if not row:
-        raise ValueError("No steps found for username")
+        raise ValueError("No step goal found for username")
 
     logger.debug(
-        "fetched steps for the user for the curr date from user_daily_steps db table"
+        "fetched target_steps for the user for the curr date from user_daily_steps db table"
+    )
+    return row[0]
+
+async def update_user_curr_steps(session: AsyncSession, username: str, curr_steps: int, date: datetime) -> None:
+    """
+    Add new curr_steps for a user for a given date. Used for the background step
+    tracker to increase daily steps
+    """
+    if curr_steps <= 0:
+        raise ValueError("steps must be positive")
+
+    query = """
+        INSERT INTO user_daily_steps (username, curr_steps, curr_date)
+        VALUES (:username, :curr_steps, :date)
+        ON CONFLICT (username, curr_date) DO UPDATE SET
+            curr_steps = user_daily_steps.curr_steps + EXCLUDED.curr_steps;
+    """
+
+    await session.execute(text(query), {"username": username, "curr_steps": curr_steps})
+
+    logger.debug("updated curr_steps record for the user in user_daily_steps db table")
+
+async def get_curr_steps_count(
+    session: AsyncSession, username: str, date: date
+) -> int:
+    """
+    Fetch the current steps for the user for the current date, Used by the frontend to
+    display the steps in user feed screen
+    """
+
+    query = """
+        SELECT curr_steps FROM user_daily_steps
+        WHERE LOWER(username) = LOWER(:username)
+            AND curr_date = :date;
+    """
+
+    result = await session.execute(
+        text(query), {"username": username.strip(), "date": date}
+    )
+
+    row = result.fetchone()
+    if not row:
+        raise ValueError("No step goal found for username")
+
+    logger.debug(
+        "fetched target_steps for the user for the curr date from user_daily_steps db table"
     )
     return row[0]
