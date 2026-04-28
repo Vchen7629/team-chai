0ef421745afe991cc54a10f2f2430b1b1aec1125
Vchen7629 <chenvincent7629@gmail.com>
2026-04-27 17:15:32 -0700

    feat(frontend): updated userProfile.tsx screen to show account data from database

--- frontend/api/user.ts
+++ frontend/api/user.ts
@@ -1,8 +1,9 @@
 import * as SecureStore from 'expo-secure-store'
 import client from './client'
+import { AccountDetailsRes, FitnessDetailRes } from '../components/profileDetailsComponents'
 
 export const UserService = {
-    fetch_account_details: async() => {
+    fetch_account_details: async(): Promise<AccountDetailsRes> => {
         const session_token = await SecureStore.getItemAsync('session_token')
 
         const response = await client.get(`/user/account_details/${session_token}`)
@@ -10,7 +11,7 @@ export const UserService = {
         return response.data
     },
 
-    fetch_fitness_data: async() => {
+    fetch_fitness_data: async(): Promise<FitnessDetailRes> => {
         const session_token = await SecureStore.getItemAsync('session_token')
 
         const response = await client.get(`/user/fitness/${session_token}`)
