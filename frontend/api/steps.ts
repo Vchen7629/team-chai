f2fa496a4d10a67625204593edbb69155008ce9a
Vchen7629 <chenvincent7629@gmail.com>
2026-04-28 10:34:31 -0700

    feat(frontend): added seperate api call for step goal and current steps

--- frontend/api/steps.ts
+++ frontend/api/steps.ts
@@ -28,16 +28,25 @@ export const StepsService = {
 
     update_user_steps: async(steps: number) => {
         const session_token = await SecureStore.getItemAsync('session_token')
+        const today = new Date().toISOString().split('T')[0]
 
-        const response = await client.post(`/steps/add`, { session_token, steps })
+        const response = await client.post(`/steps/add`, { session_token, steps, curr_date: today })
 
         return response.data
     },
 
-    fetch_user_steps: async(date: string) => {
+    fetch_user_step_goal: async(date: string) => {
         const session_token = await SecureStore.getItemAsync('session_token')
 
-        const response = await client.post(`/steps/get_user`, { session_token, date})
+        const response = await client.post(`/steps/get_goal`, { session_token, date })
+
+        return response.data
+    },
+    
+    fetch_user_curr_steps: async(date: string) => {
+        const session_token = await SecureStore.getItemAsync('session_token')
+
+        const response = await client.post(`/steps/get_curr_steps`, { session_token, date })
 
         return response.data
     }   
