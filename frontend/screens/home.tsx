b899dbf9149bd95e0b6d830323e2d2f69c4c1cbd
James Hoang <jhoang52@ucmerced.edu>
2026-04-28 15:29:58 -0700

    Added Sign Up/Login prompts + welcome message

--- frontend/screens/home.tsx
+++ frontend/screens/home.tsx
@@ -3,7 +3,23 @@ import { Text, View } from "react-native"
 const HomeScreen = () => {
     return (
         <View className="flex flex-col w-full h-full items-center justify-center">
-            <Text>Hello this is home screen</Text>
+            <View className="mb-8">
+                <Text className="text-4xl font-bold text-blue-600 mb-8 text-center">
+                    <Text>Welcome to HealthIQ</Text>
+                    <Text></Text>
+                    <Text></Text>
+                    <Text></Text>
+                </Text>
+            </View>
+
+            <View className="space-y-4 w-full items-center">
+                <Text className="text-lg text-gray-600">
+                    New User? Select <Text className="font-semibold">"Sign Up"</Text>
+                </Text>
+                <Text className="text-lg text-gray-600">
+                    Returning User? Select <Text className="font-semibold">"Login"</Text>
+                </Text>
+            </View>
         </View>
     )
 }
