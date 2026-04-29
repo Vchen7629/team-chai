8e7a0149d4bf99879943811a0212d7c611292b69
Vchen7629 <chenvincent7629@gmail.com>
2026-04-28 17:46:43 -0700

    feat(frontend): added username to auth context so it can display on userfeed screen

--- frontend/context/authContext.tsx
+++ frontend/context/authContext.tsx
@@ -4,21 +4,31 @@ import * as SecureStore from 'expo-secure-store'
 interface AuthContextType {
     loggedIn: boolean
     setLoggedIn: (value: boolean) => void
+    username: string,
+    setUsername: (value: string) => void
 }
 
-const AuthContext = createContext<AuthContextType>({ loggedIn: false, setLoggedIn: () => {} })
+const AuthContext = createContext<AuthContextType>({ 
+    loggedIn: false, setLoggedIn: () => {},
+    username: '', setUsername: () => {}
+})
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [loggedIn, setLoggedIn] = useState(false)
+    const [loggedIn, setLoggedIn] = useState<boolean>(false)
+    const [username, setUsername] = useState<string>('')
 
     useEffect(() => {
-        SecureStore.getItemAsync('session_token').then(token => {
+        Promise.all([
+            SecureStore.getItemAsync('session_token'),
+            SecureStore.getItemAsync('username')
+        ]).then(([token, storedUsername]) => {
             setLoggedIn(!!token);
+            if (storedUsername) setUsername(storedUsername)
         })
     }, []);
 
     return (
-        <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+        <AuthContext.Provider value={{ loggedIn, setLoggedIn, username, setUsername }}>
             {children}
         </AuthContext.Provider>
     )
